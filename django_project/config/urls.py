505f6bb63bb81b6abc2fd911411354701a28d0db
AlienJ <alongejoshua@gmail.com>
2024-06-17 11:36:49 +0100

    working with django basis

--- django_project/config/urls.py
+++ django_project/config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from my_app.views import index, hello
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('hello/<str:first_name>/', hello)
 ]
--- django_project/my_app/views.py
+++ django_project/my_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello World")
+
+def hello(request, first_name:str) -> str:
+    return HttpResponse(f"Hello {first_name}")
+
