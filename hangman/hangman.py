e3302766382da285eca47cf2297076180603084b
AlienJ <alongejoshua@gmail.com>
2024-06-23 23:59:59 +0100

    adds description

---
+++ hangman/hangman.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+
+""" HANGMAN PROJECT
+The game randomly selects a word from a predefined list of words.
+    The player is prompted to guess a letter, and the game indicates
+    whether the letter appears in the word or not. The game continues
+    until the player correctly guesses the word or runs out of attempts.
+git
+"""
