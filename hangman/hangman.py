2944300c1aecdb35fdd58b485a0b3e11d3fa87a5
AlienJ <alongejoshua@gmail.com>
2024-06-24 23:53:48 +0100

    flowchart for the hangman process

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,9 +1,10 @@
 #!usr/bin/python3
 
-""" HANGMAN PROJECT
+""" 
+HANGMAN PROJECT
+
 The game randomly selects a word from a predefined list of words.
-    The player is prompted to guess a letter, and the game indicates
-    whether the letter appears in the word or not. The game continues
-    until the player correctly guesses the word or runs out of attempts.
-git
+The player is prompted to guess a letter, and the game indicates
+whether the letter appears in the word or not. The game continues
+until the player correctly guesses the word or runs out of attempts.
 """
