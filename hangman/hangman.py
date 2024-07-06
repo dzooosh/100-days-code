b9dc3df8184944eaaab1f088143d0484ca7aa8b5
AlienJ <alongejoshua@gmail.com>
2024-07-06 05:44:21 +0100

    "Refactored hangman game logic to include lives counter, hangman pictures, and win/lose conditions"

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -75,24 +75,27 @@ words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
   Todo
     Make dashes based on the number of letters present in the RANDOM word
     Loop continuously till all life is lost or the word is answered correctly
-    For any right word replace dash with the letters
 """
 
-# Generate a random word
+# Pick a random word
 random_word = random.choice(words)
 print(f'Word picked: {random_word}')
 
 # Generate as many blanks as letters in word
-# put random word in a list
 # initial list contains the words length in dashes e.g hawk ['_', '_', '_', '_']
-# keep trying until the answer is right 
+# put random word in a list
 display = []
+
 # create another list with the right word in a list
 # e.g hawk ['h', 'a', 'w', 'k']
 word = []
+# Number of lives used. Also helps with the hangman pics
+LIFE = 0
+
+for letter in random_word:
+  word.append(letter)
 
 for letter in range(len(random_word)):
-  word.append(letter)
   display += "_"
 
 # continue to loop till answer is right
@@ -100,15 +103,31 @@ word_length = len(random_word)
 
 # creating number of lives (6) for trial - after the 6 its game over
 while (display != word):
+  print(HANGMANPICS[LIFE])
   # Ask the user to guess a letter
   guess = input("Guess a letter ?").lower()
 
   # if letter guess in random_word fill the blanks
-  # else lost one life
-
+  # else lose a life
   for position in range(word_length):
     letter = random_word[position]
     if guess == letter:
       display[position] = letter
+      
+  # check if guess is not present in the display word
+  if guess not in display:
+    # adding because of the list sequence
+    LIFE += 1
 
+  # check if you've used up the 6 guesses
+  print(LIFE)
+
+  if LIFE == 6:
+    break
+  
   print(display)
+
+if display != word:
+  print("You Died")
+else:
+  print("You Win")
